34ca5fe0b5ba06c784e98a2de873f08a1b58f00f
yuto-0925 <yutoyuto0925@gmail.com>
2021-06-26 17:19:00 +0900

    Chan.

--- insta/urls.py
+++ insta/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = 'insta'
 urlpatterns = [
-    path(r'', views.index, name='index'),
-    # 以下を追記(views.pyのcall_write_data()にデータを送信できるようにする)
-    path("ajax/", views.call_write_data, name="call_write_data"),
+    path('', views.templates, name='templates'),
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -5,6 +5,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from insta import views
+
 urlpatterns = [
+    path('', views.templates, name='templates'),
     path('admin/', admin.site.urls),
 ]
