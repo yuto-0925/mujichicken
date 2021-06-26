5fd260b022627ed0bab9a0d8b499e0776d53b694
yuto-0925 <yutoyuto0925@gmail.com>
2021-06-26 17:07:15 +0900

    Changed th.

--- insta/views.py
+++ insta/views.py
@@ -1,16 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# application/write_data.pyをインポートする
-from .application import write_data
 
-# Create your views here.
-def index(req):
-    return render(req, 'templates.html')
-
-# ajaxでurl指定したメソッド
-def call_write_data(req):
-    if req.method == 'GET':
-        # write_data.pyのwrite_csv()メソッドを呼び出す。
-        # ajaxで送信したデータのうち"input_data"を指定して取得する。
-        write_data.write_csv(req.GET.get("input_data"))
-        return HttpResponse()
+def post_list(request):
+    return render(request, 'insta/templates.html', {})
